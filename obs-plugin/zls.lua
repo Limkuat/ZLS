5e62e4fa6400e0987b175cc4b0f3301b2d573b52
Amaury Carrade <amaury@carrade.eu>
2022-03-16 19:43:32 +0100

    Still first plugin tests

--- obs-plugin/zls.lua
+++ obs-plugin/zls.lua
@@ -1,47 +1,41 @@
-print("Start Plugin ZLS Input :|")
-
 obs = obslua
-
-local dir = obs.os_opendir("/home/amaury/Programmes/MyOpenBackpack/zls/conciliator/segs")
-print(dir)
-local entry
-repeat
-    entry = obs.os_readdir(dir)
-    if entry then
-        print(entry.d_name)
-    end
-until not entry
-obs.os_closedir(dir)
 
 local source_def = {}
 source_def.id = "zls_input_source"
 source_def.type = obslua.OBS_SOURCE_TYPE_INPUT
-source_def.output_flags = bit.bor(obslua.OBS_SOURCE_VIDEO, obslua.OBS_SOURCE_AUDIO)
+source_def.output_flags = bit.bor(obslua.OBS_SOURCE_ASYNC_VIDEO, obslua.OBS_SOURCE_AUDIO)
 
 source_def.get_name = function()
-    return "ZLS Input Filesystem"
+    return "ZLS Input"
 end
 
 source_def.create = function(settings, source)
     -- typically source data would be stored as a table
     local my_source_data = {}
-    -- TODO use video size instead
-    my_source_data.width = 400
-    my_source_data.height = 400
+    my_source_data.width = 640
+    my_source_data.height = 480
+
+    local path = script_path() .. '../sources/2.mkv.raw.y420'
+    local file = loadfile(path, 'rb')
+    local size = 27648000
+    local frame_size = size / 60
+
+    --local frame
+    --repeat
+    --
+    --until not frame
+
+    local frame = {}
+    frame.width = my_source_data.width
+    frame.height = my_source_data.height
+    frame.format = obs.VIDEO_FORMAT_I420
+    frame.linesize = {}
+    frame.timestamp = obs.os_gettime_ns()
+    frame.data = { file }
+
+    obs.obs_source_output_video(source, frame)
+
     return my_source_data
-end
-
-source_def.video_render = function(my_source_data, effect)
-end
-
-source_def.get_width = function(my_source_data)
-    -- assuming the source data contains a 'width' key
-    return my_source_data.width
-end
-
-source_def.get_height = function(my_source_data)
-    -- assuming the source data contains a 'height' key
-    return my_source_data.height
 end
 
 -- register the source
